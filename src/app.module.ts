ed6890e667aa289baed5d4ba6c06cf292620ef4a
Hscomp <hscomp2002@gmail.com>
2022-06-14 13:39:22 +0430

    Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
-import { IntrospectAndCompose } from '@apollo/gateway';
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
 import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
-import { Module } from '@nestjs/common';
+import { HttpException, Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    HttpModule,
     GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
       driver: ApolloGatewayDriver,
       gateway: {
@@ -18,10 +19,33 @@ import { ConfigModule } from '@nestjs/config';
             { name: 'crm', url: process.env.CRM_GQL_ADDRESS },
           ],
         }),
+        // buildService({ name, url }) {
+        //   return new RemoteGraphQLDataSource({
+        //     url,
+        //     willSendRequest({ request, context }) {
+        //       request.http.headers.set('user-id', "12")
+        //     }
+        //   })
+        // }
+        buildService: ({ name, url }) => {
+          return new RemoteGraphQLDataSource({
+            url,
+            async willSendRequest({ request, context }: any) {
+              if (
+                request.query ===
+                'query __ApolloGetServiceDefinition__ { _service { sdl } }'
+              )
+                return;
+              const token = context.req?.headers['authorization']?.split(' ')[1];
+              if (!token) throw new HttpException('You need to pass authorization header', 403);
+              if (!await AppService.auth(token))  throw new HttpException('unauthorized access', 406);
+            },
+          });
+        },
       },
     }),
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  static async auth(token: string): Promise<boolean> {
+    console.log(`checking token : ${token}`);
+    const query = `{
+      gettest(token: "${token}")
+    }`;
+    const res = await axios.post(process.env.ACADEMY_GQL_ADDRESS, {
+      query,
+    });
+    return Boolean(res.data.data.gettest);
   }
 }
